79e18043b580f4f5763a10c0781130faa702fef6
NulledMiku <aaron12-13@hotmail.co.uk>
2015-08-08 21:14:04 +0100

    Miku Spammer V2.0
    Total rewrite of chat spam.
    Added a /all toggle

--- Miku Spammer/Program.cs
+++ Miku Spammer/Program.cs
@@ -1,22 +1,19 @@
-﻿namespace UniversalSpammerRebornAndresurrected
+﻿//MikuSpammer :wub:
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace UniversalSpammerRebornAndresurrected
 {
     using System;
-
     using LeagueSharp;
     using LeagueSharp.Common;
 
-    /// <summary>
-    /// Emote Spammer Made By TheKushStyle (https://github.com/TheKushStyle)- Updated For Mastery Emote By Miku
-    /// Chat Spammer completely made by Miku (https://github.com/aaronspong)
-    /// Shoutout to lostit for the L$ Crack <3
-    /// #nno
-    /// </summary>
     internal class Program
     {
-
         private static Menu Config;
-
         private double tick;
+        private static string test = "test complete";
+        private static string all = " ";
 
         private static void Main(string[] args)
         {
@@ -27,7 +24,7 @@
         {
             Config = new Menu("Spammer", "EmoteSpammer", true);
 
-            //Emote Spam
+            // Emote Spam Menu
             Config.AddSubMenu(new Menu("Emotes", "Emotes"));
             Config.SubMenu("Emotes")
                 .AddItem(new MenuItem("EmotePress", "Emote On Key press"))
@@ -38,14 +35,24 @@
             Config.SubMenu("Emotes").AddItem(new MenuItem("Type", "Which Emote to spam?")).SetValue(new Slider(1, 4, 1));
             Config.SubMenu("Emotes").AddItem(new MenuItem("kappa", "1 = laugh, 2 = Taunt, 3 = Joke, 4 = Mastery"));
 
-            //Chat Spam
+            // Chat Spam Menu
             Config.AddSubMenu(new Menu("Chat", "Chat"));
             Config.SubMenu("Chat")
                 .AddItem(new MenuItem("Press Button", "Press Button"))
                 .SetValue(new KeyBind(32, KeyBindType.Press));
             Config.SubMenu("Chat")
+                .AddItem(new MenuItem("Random Shitty Name", "Random Shitty Name"))
+                .SetValue(new KeyBind(32, KeyBindType.Toggle));
+            Config.SubMenu("Chat")
                 .AddItem(new MenuItem("ChatType", "Who are you?"))
-                .SetValue(new StringList(new[] {"Radi", "Royals", "Miku", "Killer", "Tim", "Sweden", "Taco", "Eldiath", "Emenike", "Finn", "Oxide", "LostIt", "Broly", "Ian", "Nathan", "Tahm", "Alfie"}, 0));
+                .SetValue(
+                    new StringList(
+                        new[]
+                        {
+                            "Radi", "Royals", "Miku", "Killer", "Tim", "Sweden", "Taco", "Eldiath", "Emenike", "Finn",
+                            "Oxide", "LostIt", "Broly", //"Ian", "Nathan", "Tahm", "Alfie", "Rhys"
+                        },
+                        0));
 
             Config.AddToMainMenu();
 
@@ -54,7 +61,7 @@
 
         private static void OnUpdate(EventArgs args)
         {
-            double tick = 0;
+                double tick = 0;
             tick = TimeSpan.FromSeconds(Environment.TickCount).Minutes;
 
             if (ObjectManager.Player.HasBuff("Recall"))
@@ -71,112 +78,107 @@
                 {
                     SPAM();
                 }
-                
+
                 if (Config.Item("Press Button").GetValue<KeyBind>().Active)
                 {
                     Chatspam();
                 }
+
             }
         }
 
+        //TheEmoteSpam
         private static void SPAM()
         {
-            if (Config.Item("Type").GetValue<Slider>().Value == 1)
+            switch (Config.Item("Type").GetValue<StringList>().SelectedIndex)
             {
-                Game.Say("/l");
-            }
-
-            if (Config.Item("Type").GetValue<Slider>().Value == 2)
-            {
-                Game.Say("/t");
-            }
-
-            if (Config.Item("Type").GetValue<Slider>().Value == 3)
-            {
-                Game.Say("/j");
-            }
-
-            if (Config.Item("Type").GetValue<Slider>().Value == 4)
-            {
-                Game.Say("/masterybadge");
+                case 0:
+                    Game.Say("/l");
+                    break;
+                case 1:
+                    Game.Say("/t");
+                    break;
+                case 2:
+                    Game.Say("/j");
+                    break;
+                case 3:
+                    Game.Say("/masterybadge");
+                    break;
             }
         }
 
+        //The chatspam.
         private static void Chatspam()
         {
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Radi")
             {
-                Game.Say("/all Kurwa Radi");
-            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Miku")
-            {
-                Game.Say("/all Desu");
-            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Royals")
-            {
-                Game.Say("/all Suck My Balls");
-            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Killer")
-            {
-                Game.Say("/all I can only play with relax!");
-            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Tim")
-            {
-                Game.Say("/all Go back to SECenter");
-            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Sweden")
-            {
-                Game.Say("/all Nein!");
-            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Taco")
-            {
-                Game.Say("/all Me encanta patatas fritas <3");
-                            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Eldiath")
-            {
-                Game.Say("/all :wub:");
-            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Emenike")
-            {
-                Game.Say("/all Go back to turkey <3");
-            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Finn")
-            {
-                Game.Say("/all Hab dich lieb");
-            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Oxide")
-            {
-                Game.Say("/all Leaching pleb, kurwa!");
-            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "LostIt")
-            {
-                Game.Say("/all Ty for L$ Crack <3");
-            }
-            if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Broly")
-            {
-                Game.Say("/all :thugdoge: never forget <3");
-            }
-                if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Ian")
-            {
-                Game.Say("/all Raise Your Dongers");
-            }
-                if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Nathan")
-                {
-                    Game.Say("Get Jingle Jangled");
-                }
-                if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Tahm")
-                {
-                    Game.Say("Tahm kench is cancer");
-                }
-                if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Alfie")
-                {
-                    Game.Say("Ermagawd that's so op™");
-                }
-                if (Config.Item("ChatType").GetValue<StringList>().SelectedValue == "Rhys")
-                {
-                    Game.Say("Can I touch your nipples");
-                }
 
+                if (Config.Item("Random Shitty Name").GetValue<KeyBind>().Active)
+                {
+                    all = "/all ";
+                }
+                else all = "";
+
+
+
+
+                switch (Config.Item("ChatType").GetValue<StringList>().SelectedIndex)
+                {
+                    case 0:
+                        Game.Say(all + "kurwa Radi");
+                        break;
+                    case 1:
+                        Game.Say(all + "Desu");
+                        break;
+                    case 2:
+                        Game.Say(all + "suck my balls");
+                        break;
+                    case 3:
+                        Game.Say(all + "I can only play with relax!");
+                        break;
+                    case 4:
+                        Game.Say(all + "Go back to SECenter");
+                        break;
+                    case 5:
+                        Game.Say(all + "Nein!");
+                        break;
+                    case 6:
+                        Game.Say(all + "Me encanta patatas fritas <3");
+                        break;
+                    case 7:
+                        Game.Say(all + "wub");
+                        break;
+                    case 8:
+                        Game.Say(all + "Go back to turkey");
+                        break;
+                    case 9:
+                        Game.Say(all + "Hab Dich Lieb");
+                        break;
+                    case 10:
+                        Game.Say(all + "Leeching pleb, kurwa!");
+                        break;
+                    case 11:
+                        Game.Say(all + "Ty for L$ Crack <3");
+                        break;
+                    case 12:
+                        Game.Say(all + ":thugdoge: never forget <3");
+                        break;
+                    case 13:
+                        Game.Say(all + "Raise Your Dongers");
+                        break;
+                    case 14:
+                        Game.Say(all + "Get Jingle Jangled");
+                        break;
+                    case 15:
+                        Game.Say(all + "Tahm kench is cancer");
+                        break;
+                    case 16:
+                        Game.Say(all + "Ermagawd that's so op");
+                        break;
+                    case 17:
+                        Game.Say(all + "Can I touch your nipples");
+                        break;
+                }
+            }
         }
     }
 }

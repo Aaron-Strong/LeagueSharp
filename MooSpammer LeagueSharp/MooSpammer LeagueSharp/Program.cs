53ffd15ba596261aca0c140f91a79d7079d1622f
NulledMiku <aaron12-13@hotmail.co.uk>
2015-12-16 00:45:20 +0000

    MooSpammer Release
    Ascii Spammer with text inspired by CowSay

---
+++ MooSpammer LeagueSharp/MooSpammer LeagueSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+namespace MooSpammer_LeagueSharp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            CustomEvents.Game.OnGameLoad += game_OnGameLoad;
+        }
+
+        private static void game_OnGameLoad(EventArgs args)
+        {
+
+            Game.OnInput += Game_OnGameInput;
+
+        }
+
+        private static void Game_OnGameInput(GameInputEventArgs args)
+        {
+            string command = args.Input.Split(' ').First();
+            string text = args.Input.Substring(args.Input.IndexOf(' '));
+            
+            if (args.Input.StartsWith("/"))
+            {
+                args.Process = false;
+            }
+            Console.WriteLine(args.Input);
+            Console.WriteLine(text);
+             switch (command)
+            {
+
+                case ".Cow":
+                    Game.Say(@". _________________");
+                    Game.Say($"( {text} )");
+                    Game.Say(@". -----------------");
+                    Game.Say(@".        o   ^__^");
+                    Game.Say(@".         o  (oO)\_______");
+                    Game.Say(@".            (__)\       )\/\");
+                    Game.Say(@".             U  ||----w |");
+                    Game.Say(@".                ||     ||");
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+}

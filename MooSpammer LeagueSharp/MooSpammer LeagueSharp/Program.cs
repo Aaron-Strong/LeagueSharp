94167deced79deb4f9e7a37bfc36dc041b7e4ec7
NulledMiku <aaronspong@users.noreply.github.com>
2016-03-23 09:22:54 +0000

    Update Program.cs

--- MooSpammer LeagueSharp/MooSpammer LeagueSharp/Program.cs
+++ MooSpammer LeagueSharp/MooSpammer LeagueSharp/Program.cs
@@ -32,7 +32,10 @@ namespace MooSpammer_LeagueSharp
             string text = args.Input.Substring(args.Input.IndexOf(' '));
             Console.WriteLine(args.Input);
             Console.WriteLine(text);
-
+            if (command == null)
+            {
+                Game.PrintChat("<font color='#00FFFF'>Add a spacebar dippy")
+            }
              switch (command)
             {
 
@@ -73,10 +76,10 @@ namespace MooSpammer_LeagueSharp
                     Game.Say(@"/all ./||\\---------- | /||\\/");
                     break;
                 case ".Lobby":
-                    Game.Say($"/all Welcome to Miku's Modded Lobby");
+                    Game.Say($"/all Welcome to {text}'s Modded Lobby");
                     Utility.DelayAction.Add(1000, () => Game.Say($"/all Press L3 for the Mod Menu"));
                     Utility.DelayAction.Add(2000, () => Game.Say($"/all Press R3 for All Unlocks"));
-                    Utility.DelayAction.Add(3000, () => Game.Say($"/all Follow Miku on TheTechGame & SevenSins @Miku"));
+                    Utility.DelayAction.Add(3000, () => Game.Say($"/all Follow {text} on TheTechGame & SevenSins @{text}"));
                     Utility.DelayAction.Add(4000, () => Game.Say($"/all Don't forget to donate for more modded lobbies!"));
                     Utility.DelayAction.Add(5000, () => Game.Say($"/all Aimbot Exploit Succesfully injected!"));
                     Utility.DelayAction.Add(6000, () => Game.Say($"/all +3Ba246218 exp"));

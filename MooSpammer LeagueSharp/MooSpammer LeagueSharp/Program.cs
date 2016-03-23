93f35fd56e19c646617e3c34621b31a642bbf0a1
NulledMiku <aaronspong@users.noreply.github.com>
2016-03-23 08:26:34 +0000

    More Memes Soon
    moo

--- MooSpammer LeagueSharp/MooSpammer LeagueSharp/Program.cs
+++ MooSpammer LeagueSharp/MooSpammer LeagueSharp/Program.cs
@@ -72,6 +72,16 @@ namespace MooSpammer_LeagueSharp
                     Game.Say(@"/all . / /|          |  / /|");
                     Game.Say(@"/all ./||\\---------- | /||\\/");
                     break;
+                case ".Hack":
+                    Game.Say($"/all Welcome to Miku's Modded Lobby");
+                    Game.Say($"/all Press L3 for the Mod Menu");
+                    Game.Say($"/all Press R3 for All Unlocks");
+                    Game.Say($"/all Follow Miku on THeTechHame & SevenSins");
+                    Game.Say($"/all Don't forget to donate for more modded lobbies!");
+                    Game.Say($"/all Aimbot Exploit Succesfully injected!");
+                    Game.Say($"/all +3Ba246218 exp");
+                    Game.Say($"/all Don't forget to restart the game and prestige!");
+                    break;
                 default:
                     return;
             }

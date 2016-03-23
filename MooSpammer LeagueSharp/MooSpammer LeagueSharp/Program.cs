80b92b9eb86625f8a9e3b796000ea19bf20a36c0
NulledMiku <aaronspong@users.noreply.github.com>
2016-03-23 09:15:39 +0000

    Update Program.cs

--- MooSpammer LeagueSharp/MooSpammer LeagueSharp/Program.cs
+++ MooSpammer LeagueSharp/MooSpammer LeagueSharp/Program.cs
@@ -23,7 +23,7 @@ namespace MooSpammer_LeagueSharp
         {
 
             Game.OnInput += Game_OnGameInput;
-
+            
         }
 
         private static void Game_OnGameInput(GameInputEventArgs args)
@@ -72,15 +72,20 @@ namespace MooSpammer_LeagueSharp
                     Game.Say(@"/all . / /|          |  / /|");
                     Game.Say(@"/all ./||\\---------- | /||\\/");
                     break;
-                case ".Hack":
+                case ".Lobby":
                     Game.Say($"/all Welcome to Miku's Modded Lobby");
-                    Game.Say($"/all Press L3 for the Mod Menu");
-                    Game.Say($"/all Press R3 for All Unlocks");
-                    Game.Say($"/all Follow Miku on THeTechHame & SevenSins");
-                    Game.Say($"/all Don't forget to donate for more modded lobbies!");
-                    Game.Say($"/all Aimbot Exploit Succesfully injected!");
-                    Game.Say($"/all +3Ba246218 exp");
-                    Game.Say($"/all Don't forget to restart the game and prestige!");
+                    Utility.DelayAction.Add(1000, () => Game.Say($"/all Press L3 for the Mod Menu"));
+                    Utility.DelayAction.Add(2000, () => Game.Say($"/all Press R3 for All Unlocks"));
+                    Utility.DelayAction.Add(3000, () => Game.Say($"/all Follow Miku on TheTechGame & SevenSins @Miku"));
+                    Utility.DelayAction.Add(4000, () => Game.Say($"/all Don't forget to donate for more modded lobbies!"));
+                    Utility.DelayAction.Add(5000, () => Game.Say($"/all Aimbot Exploit Succesfully injected!"));
+                    Utility.DelayAction.Add(6000, () => Game.Say($"/all +3Ba246218 exp"));
+                    Utility.DelayAction.Add(7000, () => Game.Say($"/all Don't forget to restart the game to prestige!"));
+                    Utility.DelayAction.Add(8000, () => Game.Say($"/all This lobby is sponsored by GFuel!"));
+                    Utility.DelayAction.Add(9000, () => Game.Say($"/all To find out more visit their website at"));
+                    Utility.DelayAction.Add(10000, () => Game.Say($"/all www.gfuel.com"));
+                    Utility.DelayAction.Add(11000, () => Game.Say($"/all And incase you get banned we also sell accounts at"));
+                    Utility.DelayAction.Add(12000, () => Game.Say($"/all www.moo.tokyo"));
                     break;
                 default:
                     return;

532e4e42c12d3088c7b5621c8212e78e7be3ec5f
NulledMiku <aaron12-13@hotmail.co.uk>
2016-05-28 15:57:16 +0100

    Stop typing in chat thingy

---
+++ DisableChat/DisableChat/Program.cs
@@ -0,0 +1,28 @@
+﻿//hawk didn't want to talk in chat
+using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+namespace DisableChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += game_OnGameLoad;
+        }
+
+        private static void game_OnGameLoad(EventArgs args)
+        {
+            Game.OnInput += Game_OnGameInput;
+        }
+
+        private static void Game_OnGameInput(GameInputEventArgs args)
+        {
+            if (args.Input == "/ff" || args.Input == "/surrender")
+            {
+                Game.Say("/ff");
+            }
+            else Game.Say("");
+        }
+    }
+}

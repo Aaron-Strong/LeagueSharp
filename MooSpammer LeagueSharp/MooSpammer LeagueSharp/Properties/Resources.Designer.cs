5de6d0663cf492bb2694fe8a70211b5f2c5a3f61
NulledMiku <aaron12-13@hotmail.co.uk>
2016-06-12 03:41:01 +0100

    CUSTOM SPAM SAVING!!!
    Use .customspam  or .customsave to create your own saved spam.
    Access it from the menu!
    Spam it with your own hotkey!

--- MooSpammer LeagueSharp/MooSpammer LeagueSharp/Properties/Resources.Designer.cs
+++ MooSpammer LeagueSharp/MooSpammer LeagueSharp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MooSpammer_LeagueSharp.Properties {
+namespace MooSpammer.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MooSpammer_LeagueSharp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MooSpammer_LeagueSharp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MooSpammer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

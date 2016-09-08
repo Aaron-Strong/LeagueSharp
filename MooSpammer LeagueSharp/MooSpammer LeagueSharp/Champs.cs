d3017944860f5f075644662a2f3c36bb1a9a034c
Mooku <aaron12-13@hotmail.co.uk>
2016-09-08 15:28:41 +0100

    Revert "Revert "Clean up""
    This reverts commit 6b800465c5a8bb32f19828acf2d8a307d4467ff1.

--- MooSpammer LeagueSharp/MooSpammer LeagueSharp/Champs.cs
+++ MooSpammer LeagueSharp/MooSpammer LeagueSharp/Champs.cs
@@ -156,28 +156,25 @@ namespace MooSpammer
         public static void Init()
         {
             CreateDir();
-            //WriteJson();
         }
 
         static private void CreateDir()
         {
-            if (! Directory.Exists(MooSpammerDir))
+            if (!Directory.Exists(MooSpammerDir) && !Directory.Exists(ChampionsDir) && !Directory.Exists(CustomDir))
             {
-                Console.WriteLine("[MooSpamer] MooSpammerDir Does Not Exist, Creating...");
-                Directory.CreateDirectory(MooSpammerDir);
-                Console.WriteLine("[MooSpamer] MooSPammerDir Created");
+                Game.PrintChat("[MooSpammer] First time loaded...");
+                Console.WriteLine("[MooSpammer] First time loaded...");
+                Game.PrintChat($"[MooSpammer] Creating files in {MooSpammerDir}");
+                Console.WriteLine($"[MooSpammer] Creating files in {MooSpammerDir}");
+                MakeFolder(MooSpammerDir);
+                MakeFolder(ChampionsDir);
+                MakeFolder(CustomDir);
             }
-            if (!Directory.Exists(ChampionsDir))
+            else
             {
-                Console.WriteLine("[MooSpamer] ChampionsDir Does Not Exist, Creating...");
-                Directory.CreateDirectory(ChampionsDir);
-                Console.WriteLine("[MooSpamer] ChampionsDir Created");
-            }
-            if (!Directory.Exists(CustomDir))
-            {
-                Console.WriteLine("[MooSpamer] CustomDir Does Not Exist, Creating...");
-                Directory.CreateDirectory(CustomDir);
-                Console.WriteLine("[MooSpamer] Created CustomDir");
+                if (!Directory.Exists(MooSpammerDir)) MakeFolder(MooSpammerDir);
+                if (!Directory.Exists(ChampionsDir)) MakeFolder(ChampionsDir);
+                if (!Directory.Exists(CustomDir)) MakeFolder(CustomDir);
             }
             foreach (var champ in champNames)
             {
@@ -186,7 +183,6 @@ namespace MooSpammer
                     Console.WriteLine("[MooSpamer] File for " + champ + " not found, creating...");
                     File.Create(Path.Combine(ChampionsDir, champ + ".json"));
                     Console.WriteLine("[MooSpamer]  for " + champ + " created");
-                    Console.WriteLine("[MooSpamer] Created file for" + champ);
                 }
             }
             if (!File.Exists(Path.Combine(CustomDir, "default.json")))
@@ -194,20 +190,17 @@ namespace MooSpammer
                 Console.WriteLine("[MooSpamer] Default custom file not found, creating...");
                 File.Create(Path.Combine(CustomDir, "default.json"));
                 Console.WriteLine("[MooSpamer] Default custom file created");
-            }
-            Game.PrintChat("[MooSpamer] Finished checking files!");
-            Game.PrintChat("[MooSpamer] Files located at...");
-            Game.PrintChat(MooSpammerDir);
-            Console.WriteLine("[MooSpamer] Files located at...");
-            Console.WriteLine(MooSpammerDir);
+            } 
         }
-        
-         /*
-        static private void WriteJson()
-        {
 
+        private static void MakeFolder(string Dir)
+        {
+            Console.WriteLine($"[MooSpamer] {Dir} Does Not Exist, Creating...");
+            Directory.CreateDirectory(Dir);
+            Console.WriteLine($"[MooSpamer] {Dir} Created");
         }
-        */
+
+
 
         static public Dictionary<string, string> LoadJsonChamps()
         {
@@ -215,8 +208,8 @@ namespace MooSpammer
             if (new FileInfo(ChampionsDir + "\\" +  currentChamp  + ".json").Length == 0)
             {
                 Game.PrintChat($"[MooSpamer] Champion {ObjectManager.Player.ChampionName} not yet Supported");
-                Game.PrintChat("[MooSpamer] Feel free to add spam to the github!");
-                Game.PrintChat("[MooSpamer] Till then... i guess you got nothing special");
+                //Game.PrintChat("[MooSpamer] Feel free to add spam to the github!");
+                //Game.PrintChat("[MooSpamer] Till then... i guess you got nothing special");
                 return null;
             }
             else
@@ -253,22 +246,31 @@ namespace MooSpammer
                 }
                 else if (filename.EndsWith(".json"))
                 {
-                    if (allFiles.Length == 1) { 
-                        using (StreamReader r = new StreamReader(filename))
+                    if (allFiles.Length == 1) {
+                        try
                         {
-                            string json = r.ReadToEnd();
-                            try
+                            using (StreamReader r = new StreamReader(filename))
                             {
-                                Dictionary<string, string> Spam = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
-                                return Spam;
+                                string json = r.ReadToEnd();
+                                try
+                                {
+                                    Dictionary<string, string> Spam = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                                    return Spam;
+                                }
+                                catch (Exception Ex)
+                                {
+                                    Game.PrintChat("[MooSpamer] ERROR - Json file is in the wrong format!");
+                                    Game.PrintChat("[MooSpamer] Turning off CustomJson menu");
+                                    Game.PrintChat("[MooSpamer] See \"Example.json\" for formatting help!");
+                                    Console.WriteLine(Ex);
+                                    return null;
+                                }
                             }
-                            catch (Exception)
-                            {
-                                Game.PrintChat("[MooSpamer] ERROR - Json file is in the wrong format!");
-                                Game.PrintChat("[MooSpamer] Turning off CustomJson menu");
-                                Game.PrintChat("[MooSpamer] See \"Example.json\" for formatting help!");
-                                return null;
-                            }
+                        }
+                        catch (Exception Ex)
+                        {
+                            Console.WriteLine("[MooSpammer] Exception whilst making custom json: " + Ex);
+                            Game.PrintChat("<font color='#ff0000'>Something went wrong with MooSpammer. Press F5(reload hotkey) to fix!</font>");
                         }
                     }
                     else
